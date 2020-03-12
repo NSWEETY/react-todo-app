5be2358ca3aabaef47bf99468a32f4108ba779bd
NSWEETY <naiksweety2000@gmail.com>
2020-03-12 12:17:59 +0530

    node

---
+++ node-todo-api/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = express.Router();
+ 
+router.get('/', (req, res) => res.send('Hello World!'));
+ 
+router.route('/todos')
+  .get((req, res) => {
+      res.json([
+         {
+           "_id": "a",
+           "text": "Item 1",
+           "done": false
+         },
+         {
+           "_id": "b",
+           "text": "Item 2",
+           "done": false
+         },
+         {
+           "_id": "c",
+           "text": "Item 3",
+           "done": true
+         },
+         {
+           "_id": "d",
+           "text": "Item 4",
+           "done": false
+         },
+      ]);
+  })
+ 
+app.use('/', router);
+ 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
